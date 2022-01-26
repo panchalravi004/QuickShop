2c14bfbad6165828a5e53ccf92a166147b9f687b
Ravi Panchal <panchalravi018@gmail.com>
2022-01-26 15:18:15 +0530

    Day1 Sign Up Complete

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,12 +1,144 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 class SignUp extends StatelessWidget {
-  const SignUp({ Key? key }) : super(key: key);
+  const SignUp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Sign Up"),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 180,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: "Enter First Name",
+                      labelText: "First Name",
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Container(
+                  width: 180,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: "Enter Last Name",
+                      labelText: "Last Name",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: "Enter Your Email",
+                labelText: "Your Email",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: "Enter Your Mobile Number",
+                labelText: "Your Number",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 180,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: "Enter Your Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Container(
+                  width: 180,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: "Enter Your Password",
+                      labelText: "Confirm Password",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 180,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: "Enter Pin Code",
+                      labelText: "Pin Code",
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Container(
+                  width: 180,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: "Enter Referral Code (Optional)",
+                      labelText: "Referral Code ?",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Container(
+              width: 110,
+              height: 35,
+              decoration: BoxDecoration(
+                  color: Colors.red, borderRadius: BorderRadius.circular(35)),
+              child: Center(
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
