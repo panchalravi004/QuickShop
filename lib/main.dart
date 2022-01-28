63e2dd96ba158897855814e00adab9942c840a6d
Ravi Panchal <panchalravi018@gmail.com>
2022-01-28 21:15:58 +0530

    Day3 With Firebase connect and etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors, avoid_print, use_key_in_widget_constructors
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quick_shop/pages/home_page.dart';
 import 'package:quick_shop/pages/login_page.dart';
@@ -5,24 +8,35 @@ import 'package:quick_shop/pages/login_page.dart';
 import 'package:quick_shop/utils/routes.dart';
 
 void main(List<String> args) {
-  runApp(const QuickShop());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(QuickShop());
 }
+
 class QuickShop extends StatelessWidget {
-  const QuickShop({ Key? key }) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.light(),
-
-      debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.loginroutes,
-      routes: {
-        //"/" : (context) => const WellCome(),
-        MyRoutes.loginroutes : (context) => const LoginPage(),
-        MyRoutes.homeroutes : (context) => const HomePage(),
-        
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print("Something Went Worng");
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.light(),
+            debugShowCheckedModeBanner: false,
+            initialRoute: MyRoutes.loginroutes,
+            routes: {
+              //"/" : (context) => const WellCome(),
+              MyRoutes.loginroutes: (context) => const LoginPage(),
+              MyRoutes.homeroutes: (context) => const HomePage(),
+            },
+          );
+        }
+        return CircularProgressIndicator();
       },
     );
   }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,143 +1,304 @@
-// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_print, non_constant_identifier_names
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formkey = GlobalKey<FormState>();
+
+  final fnamecontrol = TextEditingController();
+  final lnamecontrol = TextEditingController();
+  final emailcontrol = TextEditingController();
+  final mobilecontrol = TextEditingController();
+  final pwdcontrol = TextEditingController();
+  final cpwdcontrol = TextEditingController();
+  final pincontrol = TextEditingController();
+  final reffercontrol = TextEditingController();
+
+  var fname = "";
+  var lname = "";
+  var email = "";
+  var mobile = "";
+  var pwd = "";
+  var cpwd = "";
+  var pin = "";
+  var reffer = "";
+//show alert dialog box
+  String msgs = "Sign Up Successfully";
+  String err = "Faild to sign up";
+
+  Future<void> msg(String msg) {
+    return showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Thank You"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[Text(msg)],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(), child: Text("OK"))
+          ],
+        );
+      },
+    );
+  } 
+
+//addding student
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  Future<void> addUser() { 
+    return users
+        .doc(mobile)
+        .set({
+          'fname': fname,
+          'lname': lname,
+          'email': email,
+          'mobile': mobile,
+          'pwd': pwd,
+          'cpwd': cpwd,
+          'pin': pin,
+          'reffer':reffer
+        })
+        .then((value) => msg(msgs))
+        .catchError((error) => msg(err + " $error"));
+  }
+
+  clearText() {
+    fnamecontrol.clear();
+    lnamecontrol.clear();
+    emailcontrol.clear();
+    mobilecontrol.clear();
+    pwdcontrol.clear();
+    cpwdcontrol.clear();
+    pincontrol.clear();
+    reffercontrol.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 180,
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter First Name",
-                      labelText: "First Name",
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 180,
+                      child: TextFormField(
+                        controller: fnamecontrol,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Enter First Name",
+                          labelText: "First Name",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter First Name';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                  ),
-                ),
-                SizedBox(width: 15),
-                Container(
-                  width: 180,
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter Last Name",
-                      labelText: "Last Name",
+                    SizedBox(width: 15),
+                    Container(
+                      width: 180,
+                      child: TextFormField(
+                        controller: lnamecontrol,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Enter Last Name",
+                          labelText: "Last Name",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Last Name';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: TextFormField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: "Enter Your Email",
-                labelText: "Your Email",
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: TextFormField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: "Enter Your Mobile Number",
-                labelText: "Your Number",
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 180,
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter Your Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                ),
-                SizedBox(width: 15),
-                Container(
-                  width: 180,
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter Your Password",
-                      labelText: "Confirm Password",
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 180,
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter Pin Code",
-                      labelText: "Pin Code",
-                    ),
-                  ),
-                ),
-                SizedBox(width: 15),
-                Container(
-                  width: 180,
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter Referral Code (Optional)",
-                      labelText: "Referral Code ?",
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Container(
-              width: 110,
-              height: 35,
-              decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(35)),
-              child: Center(
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500),
+                  ],
                 ),
               ),
-            ),
-          )
-        ],
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: TextFormField(
+                  controller: emailcontrol,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    labelText: "Your Email",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: TextFormField(
+                  controller: mobilecontrol,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Mobile Number",
+                    labelText: "Your Number",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please Enter Mobile Number';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 180,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: pwdcontrol,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Enter Your Password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Password ';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Container(
+                      width: 180,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: cpwdcontrol,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Enter Your Password",
+                          labelText: "Confirm Password",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Confirm Password';
+                          } else if (value != pwdcontrol.text) {
+                            return 'Password Does Not Match';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 180,
+                      child: TextFormField(
+                        controller: pincontrol,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Enter Pin Code",
+                          labelText: "Pin Code",
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Pin Code';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Container(
+                      width: 180,
+                      child: TextFormField(
+                        controller: reffercontrol,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Enter Referral Code (Optional)",
+                          labelText: "Referral Code ?",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: InkWell(
+                  onTap: () {
+                    if (_formkey.currentState!.validate()) {
+                      setState(() {
+                        fname = fnamecontrol.text;
+                        lname = lnamecontrol.text;
+                        email = emailcontrol.text;
+                        mobile = mobilecontrol.text;
+                        pwd = pwdcontrol.text;
+                        cpwd = cpwdcontrol.text;
+                        pin = pincontrol.text;
+                        reffer = reffercontrol.text;
+                        addUser();
+                        clearText();
+                      });
+                    }
+                  },
+                  child: Container(
+                    width: 110,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(35)),
+                    child: Center(
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
