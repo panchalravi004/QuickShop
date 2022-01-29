92da9942d8e6c7cce87e13eb24114f92485d70d0
Ravi Panchal <panchalravi018@gmail.com>
2022-01-29 18:43:50 +0530

    Day5 AppBar New File Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, override_on_non_overriding_member
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_shop/widget/app_bar.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatefulWidget{
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -17,144 +18,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        toolbarHeight: 145,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(15),
-              bottomRight: Radius.circular(15),
-            ),
-            gradient: LinearGradient(
-                colors: [c1,c2],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Builder(builder: (BuildContext context) {
-                      return IconButton(
-                          onPressed: () {
-                            Scaffold.of(context).openDrawer();
-                          },
-                          icon: Icon(
-                            Icons.menu,
-                            color: Colors.white,
-                            size: 30,
-                          ));
-                    }),
-                    Container(
-                      width: 100,
-                      height: 35,
-                      decoration: BoxDecoration(
-                          color: Colors.red[600],
-                          border: Border.all(color: Colors.white, width: 1),
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(25),
-                              bottomRight: Radius.circular(25))),
-                      child: Center(
-                        child: Text(
-                          "Quick",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100,
-                      height: 35,
-                      child: Center(
-                        child: Text(
-                          "SHOP",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 100),
-                      child: SizedBox(
-                        width: 40,
-                        height: 40,
-                        child: Center(
-                          child: Icon(
-                            CupertinoIcons.cart,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 60),
-                  child: Text(
-                    "Online Shoping Store",
-                    style: TextStyle(color: Colors.white54),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 290,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(25),
-                              bottomLeft: Radius.circular(25),
-                            )),
-                        child: Center(
-                          child: SizedBox(
-                            width: 250,
-                            height: 35,
-                            child: TextFormField(
-                              textAlign: TextAlign.center,
-                              decoration: InputDecoration(
-                                hintText: "Search for products, Brand and more",
-                                border: InputBorder.none,
-
-                              ),
-                            )),
-                        ),
-                      ),
-                      Container(
-                        width: 60,
-                        height: 40,
-                        decoration: BoxDecoration(
-                            color: Colors.yellow,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(25),
-                              bottomRight: Radius.circular(25),
-                            )),
-                        child:Center(
-                          child: Icon(Icons.search,color: Colors.white,size: 35,),
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+      appBar: MyAppBar(),
       drawer: Drawer(),
     );
   }
--- lib/widget/app_bar.dart
+++ lib/widget/app_bar.dart
@@ -1,17 +1,165 @@
+// ignore_for_file: prefer_const_constructors, override_on_non_overriding_member
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AppBaR extends StatefulWidget {
-  const AppBaR({ Key? key }) : super(key: key);
+class MyAppBar extends StatefulWidget   implements PreferredSizeWidget{
+  const MyAppBar({Key? key}) : super(key: key);
 
   @override
-  _AppBaRState createState() => _AppBaRState();
+  _MyAppBarState createState() => _MyAppBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(145);
 }
 
-class _AppBaRState extends State<AppBaR> {
+class _MyAppBarState extends State<MyAppBar> {
+
+  Color c1 = Color(0xFF7E57C2);
+  Color c2 = Color(0xFF29B6F6);
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      toolbarHeight: 145,
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+          ),
+          gradient: LinearGradient(
+              colors: [c1, c2],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Builder(builder: (BuildContext context) {
+                    return IconButton(
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        icon: Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                          size: 30,
+                        ));
+                  }),
+                  Container(
+                    width: 100,
+                    height: 35,
+                    decoration: BoxDecoration(
+                        color: Colors.red[600],
+                        border: Border.all(color: Colors.white, width: 1),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            bottomRight: Radius.circular(25))),
+                    child: Center(
+                      child: Text(
+                        "Quick",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    height: 35,
+                    child: Center(
+                      child: Text(
+                        "SHOP",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 100),
+                    child: SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: Center(
+                        child: Icon(
+                          CupertinoIcons.cart,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 60),
+                child: Text(
+                  "Online Shoping Store",
+                  style: TextStyle(color: Colors.white54),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 290,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            bottomLeft: Radius.circular(25),
+                          )),
+                      child: Center(
+                        child: SizedBox(
+                            width: 250,
+                            height: 35,
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              decoration: InputDecoration(
+                                hintText: "Search for products, Brand and more",
+                                border: InputBorder.none,
+                              ),
+                            )),
+                      ),
+                    ),
+                    Container(
+                      width: 60,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.yellow,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(25),
+                            bottomRight: Radius.circular(25),
+                          )),
+                      child: Center(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 35,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
