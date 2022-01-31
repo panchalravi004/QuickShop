c4641667992b118da25e4fd702097181fb44a2b0
Ravi Panchal <panchalravi018@gmail.com>
2022-01-31 12:28:58 +0530

    Day7 User Profile Completed

--- lib/pages/userpage/user_profile.dart
+++ lib/pages/userpage/user_profile.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_typing_uninitialized_variables
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatefulWidget {
@@ -10,9 +12,14 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
+  Color c1 = Color.fromARGB(255, 189, 173, 252);
+  Color c2 = Color.fromARGB(255, 0, 229, 255);
+  var user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
@@ -23,9 +30,9 @@ class _UserProfileState extends State<UserProfile> {
                   bottomLeft: Radius.circular(25),
                   bottomRight: Radius.circular(25)),
               gradient: LinearGradient(
-                  colors: [Colors.amber, Colors.orange],
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft)),
+                  colors: [c1,c2],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
         ),
         title: Text(
           "Profile",
@@ -36,8 +43,292 @@ class _UserProfileState extends State<UserProfile> {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text("Ravi pancha;"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+        child: Container(
+          width: 370,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, 0),
+                blurRadius: 5,
+                spreadRadius: 0,
+              )
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [c2, Colors.blue],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                offset: Offset(0, 0),
+                                blurRadius: 10,
+                                spreadRadius: 0,
+                              )
+                            ],
+                        border: Border.all(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(50)),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      height: 45,
+                      child: Center(
+                        child: Text(
+                          "Username",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 180,
+                      height: 45,
+                      child: Center(
+                        child: StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user!.uid)
+                              .snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return LinearProgressIndicator();
+                            }
+                            return Text((snapshot.data['fname'] +
+                                    ' ' +
+                                    snapshot.data['lname'])
+                                .toString()
+                                .toUpperCase());
+                          },
+                        ),
+                        // child: Text(
+                        //   "Ravi Panchal",
+                        //   style: TextStyle(fontSize: 15),
+                        // ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      height: 45,
+                      child: Center(
+                        child: Text(
+                          "Email",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 180,
+                      height: 45,
+                      child: Center(
+                        child: StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user!.uid)
+                              .snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return LinearProgressIndicator();
+                            }
+                            return Text(snapshot.data['email']);
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      height: 45,
+                      child: Center(
+                        child: Text(
+                          "Mobile",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 180,
+                      height: 45,
+                      child: Center(
+                        child: StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user!.uid)
+                              .snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return LinearProgressIndicator();
+                            }
+                            return Text(snapshot.data['mobile']);
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      height: 45,
+                      child: Center(
+                        child: Text(
+                          "PinCode",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 180,
+                      height: 45,
+                      child: Center(
+                        child: StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user!.uid)
+                              .snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return LinearProgressIndicator();
+                            }
+                            return Text(snapshot.data['pin']);
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      height: 45,
+                      child: Center(
+                        child: Text(
+                          "Refferal !",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 180,
+                      height: 45,
+                      child: Center(
+                        child: StreamBuilder(
+                          stream: FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(user!.uid)
+                              .snapshots(),
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) {
+                              return LinearProgressIndicator();
+                            }
+                            return Text(snapshot.data['reffer']);
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.amberAccent, Colors.lightGreen],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Center(
+                        child: Text(
+                          "Edit",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
